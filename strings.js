468fe379ee6d7c5ec9e8ca09684f9c56a84a8215
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-02 15:24:35 -0600

    trying to fix stackoverflow on my count method

--- strings.js
+++ strings.js
@@ -18,9 +18,9 @@ console.log(sentence)
 
 var jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
 let aCount = 0;
-function countLetterA(str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == 'a' || str[i] == 'A')
+function countLetterA(jsHistory) {
+  for (let i = 0; i < jsHistory.length; i++) {
+    if (jsHistory[i] == 'a' || jsHistory[i] == 'A')
       aCount++
 
   }
@@ -29,8 +29,9 @@ function countLetterA(str) {
   console.log(aCount)
 
 }
-console.log(aCount)
-countLetterA(jsHistory)
+// these are producing stack overflows.... not right
+//console.log(aCount) countLetterA(jsHistory)
+// countLetterA(jsHistory)
 //3. Given the following sentence: Uppercase the entire word scream each time it appears in the sentence 
 //I SCREAM, you SCREAM, we all SCREAM for icecream
 
